a988e135d17ad08ba867fe7c03afe651d363cc86
Patrick Conroy <patrick@conroy-family.net>
2020-02-20 14:42:36 -0700

    Initial version

---
+++ jsonMessageMaker.c
@@ -0,0 +1,122 @@
+/*
+ * File:    jsonMessagemaker.c
+ * author:  patrick conroy
+ * 
+ * The goal is to crate a JSON message that has a structure like this:
+ * 
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "cjson/cJSON.h"
+#include "libepsolar.h"
+
+
+extern char    *getCurrentDateTime( void );
+
+//
+//  Quickie Macros to control Floating Point Precision
+#define FP21P(x) ( (((int)((x) * 10 + .5))/10.0) )                  // Floating Point to 1 point
+#define FP22P(x) ( (((int)((x) * 100 + .5))/100.0) )                // Floating Point to 2 points
+#define FP23P(x) ( (((int)((x) * 1000 + .5))/1000.0) )              // Floating Point to 3 points
+#define FP24P(x) ( (((int)((x) * 10000 + .5))/10000.0) )            // Floating Point to 4 points
+
+
+
+
+// -----------------------------------------------------------------------------
+static  char    currentDateTimeBuffer[ 80 ];
+char    *getCurrentDateTime (void)
+{
+    //
+    // Something quick and dirty... Fix this later - thread safe
+    time_t  current_time;
+    struct  tm  *tmPtr;
+ 
+    memset( currentDateTimeBuffer, '\0', sizeof currentDateTimeBuffer );
+    
+    /* Obtain current time as seconds elapsed since the Epoch. */
+    current_time = time( NULL );
+    if (current_time > 0) {
+        /* Convert to local time format. */
+        tmPtr = localtime( &current_time );
+ 
+        if (tmPtr != NULL) {
+            strftime( currentDateTimeBuffer,
+                    sizeof currentDateTimeBuffer,
+                    "%FT%T%z",                           // ISO 8601 Format
+                    tmPtr );
+        }
+    }
+    
+    return &currentDateTimeBuffer[ 0 ];
+}
+
+// -----------------------------------------------------------------------------
+char *realTimeDataToJSON (const char *topic, const const epsolarRealTimeData_t *rtData)
+{
+    //
+    //  Dave Gamble's C library to create JSON
+    cJSON *message = cJSON_CreateObject();
+
+    cJSON_AddStringToObject( message, "topic", topic );
+    cJSON_AddStringToObject( message, "version", "3.0" );
+    
+    //
+    //  cJSON uses a "%1.15g" format for number formatting which means we can get some
+    //  very large FP numbers in the output.  Let's round and truncate before we have cJSON
+    //  format the numbers.
+    //
+    cJSON_AddStringToObject( message, "dateTime", getCurrentDateTime() );
+    cJSON_AddStringToObject( message, "controllerDateTime", rtData-> controllerClock );
+    cJSON_AddBoolToObject( message, "isNightTime", rtData->isNightTime );
+    cJSON_AddBoolToObject( message, "loadIsOn", rtData->loadIsOn );
+    
+    cJSON_AddNumberToObject( message, "pvVoltage", FP22P( rtData->pvVoltage ) );
+    cJSON_AddNumberToObject( message, "pvCurrent", FP22P( rtData->pvCurrent ) );
+    cJSON_AddNumberToObject( message, "pvPower", FP22P( rtData->pvPower ) );
+    cJSON_AddStringToObject( message, "pvStatus", rtData->pvStatus );
+    
+    cJSON_AddNumberToObject( message, "loadVoltage", FP22P( rtData->loadVoltage ) );
+    cJSON_AddNumberToObject( message, "loadCurrent", FP22P( rtData->loadCurrent ) );
+    cJSON_AddNumberToObject( message, "loadPower", FP22P( rtData->loadPower ) );
+
+    cJSON_AddNumberToObject( message, "batterySOC", rtData->batteryStateOfCharge );
+    cJSON_AddNumberToObject( message, "batteryVoltage", FP22P( rtData->batteryVoltage ) );
+    cJSON_AddNumberToObject( message, "batteryCurrent", FP22P( rtData->batteryCurrent ) );
+    cJSON_AddStringToObject( message, "batteryStatus", rtData->batteryStatus );
+    cJSON_AddNumberToObject( message, "batteryMaxVoltage", FP22P( rtData->batteryMaxVoltage ) );
+    cJSON_AddNumberToObject( message, "batteryMinVoltage", FP22P( rtData->batteryMinVoltage ) );
+    cJSON_AddStringToObject( message, "batteryChargingStatus", rtData->batteryChargingStatus );
+    cJSON_AddNumberToObject( message, "batteryTemperature", FP22P( rtData->batteryTemperature ) );
+    
+    cJSON_AddNumberToObject( message, "controllerTemperature", FP22P( rtData->controllerTemp ) );
+    cJSON_AddStringToObject( message, "controllerStatus", rtData->controllerStatus );
+    cJSON_AddNumberToObject( message, "controllerStatusBits", rtData->controllerStatusBits );
+    
+    cJSON_AddNumberToObject( message, "energyConsumedToday", FP22P( rtData->energyConsumedToday ) );
+    cJSON_AddNumberToObject( message, "energyConsumedMonth", FP22P( rtData->energyConsumedMonth ) );
+    cJSON_AddNumberToObject( message, "energyConsumedYear", FP22P( rtData->energyConsumedYear ) );
+    cJSON_AddNumberToObject( message, "energyConsumedTotal", FP22P( rtData->energyConsumedTotal ) );
+    cJSON_AddNumberToObject( message, "energyGeneratedToday", FP22P( rtData->energyGeneratedToday ) );
+    cJSON_AddNumberToObject( message, "energyGeneratedMonth", FP22P( rtData->energyGeneratedMonth ) );
+    cJSON_AddNumberToObject( message, "energyGeneratedYear", FP22P( rtData->energyGeneratedYear ) );
+    cJSON_AddNumberToObject( message, "energyGeneratedTotal", FP22P( rtData->energyGeneratedTotal ) );
+
+    //
+    //  From the cJSON notes: Important: If you have added an item to an array 
+    //  or an object already, you mustn't delete it with cJSON_Delete. Adding 
+    //  it to an array or object transfers its ownership so that when that 
+    //  array or object is deleted, it gets deleted as well.
+    char *string = cJSON_Print( message );
+    cJSON_Delete( message );
+    
+    return string;
+}
+/*
+ */
+
---
+++ main.c
@@ -0,0 +1,186 @@
+/* 
+ * File:   main.c
+ * Author: pconroy
+ *
+ *  This program reads the data from an EPEver Solar Charge Controller and
+ *  pumps it our as a JSON formatted message to an MQTT broker
+ * 
+ * Created on February 18, 2020, 4:12 PM
+ * 
+ * Libraries:
+ *  -llibmqttrv -lepsolar -llog4c -lcjson -lmosquitto -lmodbus -lavahi-client -lavahi-common
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <getopt.h>
+#include <unistd.h>
+#include "log4c.h"
+#include "libmqttrv.h"
+#include "libepsolar.h"
+
+
+
+
+static  char    *version = "EPSOLAR_MQTT SCC Data Publisher - version 0.1";
+
+
+static  int     sleepSeconds = 60;                  // How often to send out SCC data packets
+static  char    *brokerHost = "mqttrv.local";       // default address of our MQTT broker
+static  int     loggingLevel = 3;
+
+static  char    *topTopic = "SCC";                  // MQTT top level topic
+static  char    publishTopic[ 1024 ];               // published data will be on "<topTopic>/<controlleID>/DATA"
+static  char    subscriptionTopic[ 1024 ];          // subscribe to <"<topTopic>/<controlleID>/COMMAND"
+
+static  int     synchClocks  = TRUE;
+static  int     controllerID = 1;
+static  char    *devicePortName = NULL;
+
+
+//  
+// Forwards
+static  void    parseCommandLine( int, char ** );
+extern  char    *realTimeDataToJSON( const char *publishTopic, epsolarRealTimeData_t *rtData );
+extern  void    *processInboundCommand( void * );
+
+
+
+// -----------------------------------------------------------------------------
+int main (int argc, char* argv[]) 
+{    
+    printf( "%s\n", version );
+    
+    parseCommandLine( argc, argv );
+    Logger_Initialize( "/tmp/epsolar_mqtt.log", loggingLevel );           
+    Logger_LogWarning( "%s\n", version );
+
+    
+    //
+    //  Connect to the EPSolar Solar Charge Controller
+    //  Pass in NULL to use default port defined in libepsolar/epsolar.c
+    //  Second parmeter is Modbus Slave ID.  '1' is fine
+    if (!epsolarModbusConnect( NULL, 1 )) {
+        Logger_LogFatal( "Unable to open default device port %s to connect to the solar charge controller\n", epsolarGetDefaultPortName() );
+        return( EXIT_FAILURE );
+    }
+    
+    //
+    // Create a FIFO queue for our incoming Commands over MQTT
+    //createQueue( 0, 0 );
+
+    //
+    struct mosquitto *aMosquittoInstance;
+    MQTT_ConnectRV( &aMosquittoInstance );
+    
+#if 0    
+    //
+    //  Start up a new thread to watch the Command Queue
+    pthread_t   commandProcessingThread;
+    if(pthread_create( &commandProcessingThread, NULL, processInboundCommand, NULL ) ) {
+        Logger_LogFatal( "Unable to start the command processing thread!\n" );
+        perror( "Error:" );
+        return -1;
+    }
+#endif 
+    
+
+    //
+    //  Concatenate topTopic and controller ID to create our Pub and Sub Topics
+    snprintf( publishTopic, sizeof publishTopic, "%s/%d/%s", topTopic, controllerID, "DATA" );
+    Logger_LogWarning( "Publishing messages to MQTT Topic [%s]\n", publishTopic );
+    
+    snprintf( subscriptionTopic, sizeof subscriptionTopic, "%s/%d/%s", topTopic, controllerID, "COMMAND" );
+    Logger_LogWarning( "Subscribing to commands on MQTT Topic [%s]\n", subscriptionTopic );
+    MQTT_Subscribe ( aMosquittoInstance, subscriptionTopic, 0 );
+
+        
+    //
+    //  Loop forever - read SCC data and send it out
+    epsolarRealTimeData_t   realTimeData;
+    
+    while (TRUE) {
+        if (synchClocks)
+            eps_setRealtimeClockToNow();
+        
+        //
+        //  every time thru the loop - zero out the structs!
+        memset( &realTimeData, '\0', sizeof( epsolarRealTimeData_t ) );
+        epsolarGetRealTimeData( &realTimeData );
+        
+        //
+        // craft a JSON message from the data 
+        char    *jsonMessage = realTimeDataToJSON( publishTopic, &realTimeData );
+       
+        //
+        // Publish it to our MQTT broker; QoS = 0
+        MQTT_Publish( aMosquittoInstance, publishTopic, jsonMessage, 0 );
+        
+        free( jsonMessage );
+        sleep( sleepSeconds );
+    }
+
+    
+    //
+    // we never get here!
+    MQTT_Unsubscribe( aMosquittoInstance, subscriptionTopic );
+    MQTT_Teardown( aMosquittoInstance, NULL );
+
+    //if (pthread_join( commandProcessingThread, NULL )) {
+    //    Logger_LogError( "Shutting down but unable to join the commandProcessingThread\n" );
+    //}    
+    
+    //destroyQueue();
+    
+    Logger_LogWarning( "Exiting (should not happen)" );
+    Logger_Terminate();
+    
+    return( EXIT_SUCCESS );
+}
+
+// -----------------------------------------------------------------------------
+static
+void    showHelp()
+{
+    puts( "Options" );
+    puts( "  -h  <string>   MQTT host to connect to" );
+    puts( "  -t  <string>   MQTT top level topic" );
+    puts( "  -s  N          sleep between sends <seconds>" );
+    puts( "  -i  N          give this controller an identifier (defaults to 1)" );
+    puts( "  -p  <string>   open this /dev/port to talk to contoller (defaults to /dev/ttyUSB0)" );
+    puts( "  -v  N          logging level 1..5" );
+    puts( "  -c             do NOT synch clocks (default is to synch)" );
+    exit( 1 ); 
+}
+
+// -----------------------------------------------------------------------------
+static
+void    parseCommandLine (int argc, char *argv[])
+{
+    //
+    //  Options
+    //  -h  <string>    MQTT host to connect to
+    //  -t  <string>    MQTT top level topic
+    //  -s  N           sleep between sends <seconds>
+    //  -i  <string>    give this controller an identifier (defaults to LS1024B_1)
+    //  -p  <string>    open this /dev/port to talk to contoller (defaults to /dev/ttyUSB0
+    char    c;
+    
+    while (((c = getopt( argc, argv, "h:t:s:i:p:v:j" )) != -1) && (c != 255)) {
+        switch (c) {
+            case 'h':   brokerHost = optarg;            break;
+            case 's':   sleepSeconds = atoi( optarg );  break;
+            case 't':   topTopic = optarg;              break;
+            case 'i':   controllerID = atoi( optarg );  break;
+            case 'p':   devicePortName = optarg;        break;
+            case 'v':   loggingLevel = atoi( optarg );  break;
+            case 'c':   synchClocks = FALSE;            break;
+            
+            default:    showHelp();     break;
+        }
+    }
+}
+
